c364d7f4448566d50d4e21dbdd2ea3d47b9d109c
BlackyFox <puissant@et.esiea.fr>
2015-03-03 17:07:53 +0100

    Lecture fichier vin et regression lineaire des vins

--- TP1/ToolsTP.py
+++ TP1/ToolsTP.py
@@ -1,18 +1,5 @@
 import math
 import numpy as np
-"""
-
-rendez-vous sur 	  	
-http://webia.lip6.fr/~mapsi/pmwiki.php?n=Main.TutoPython
-
-
-"""
-
-"""
-ouvrez le fichier first_step.py
-il faut maitrisier tout ce qui y est decrit 
-Une fois fait, completez less fonctions suivantes 
-"""
 
 first = np.arange(1,11)
 second = np.random.randn(10)
@@ -21,7 +8,7 @@ third = np.zeros((1,10))
 final = np.vstack((first, second, third))
 
 m7 = np.vstack((np.array([1, 2, 3]), final.T))
-print(m7)
+#print(m7)
 
 
 def tanh(x):
@@ -37,7 +24,6 @@ def sigm(x):
 def sigm_derivative(x):
   return (sigm(x)*(1-sigm(x)))
 
-print(sigm_derivative(0))
 #X a 4 colonnes, ci est la ieme colonne, renvoie 3*c1 + 2*c2 - 5*c3 + 1*c4
 def polynom(X):
   return(3*X[:,0] + 2*X[:,1] - 5*X[:,2] + X[:,3])
@@ -50,8 +36,10 @@ def rmse(predicted,observed):
 #renvoie un Vecteur de poids W tq [X.W-Y]^2 est minimum
 def linear_regression(X,Y,epsilon,nbiteration):
   w = np.random.rand(X.shape[1])
+  #print(w.shape)
   for i in range (nbiteration):
     prod = np.dot(X,w)           #multiplication de matrice
+    #print(prod.shape)
     error = rmse(Y, prod)
     print ("Erreur = ",error)
     w += epsilon*np.dot(X.T,(Y-prod))
@@ -60,4 +48,15 @@ def linear_regression(X,Y,epsilon,nbiteration):
 #generer 
 x = np.random.rand(10,4)
 y = polynom(x)
-print(linear_regression(x,y,0.01,1000))
+#print(linear_regression(x,y,0.01,1000))
+
+X = np.loadtxt("winequality-white.csv",delimiter=";")
+print(X.shape)
+
+Mat = np.ones((X.shape[0], X.shape[1]-1))
+vals = X[::, :11]
+print(vals.shape)
+notes = X[::, 11:].ravel()
+print(notes)
+print("\n*********START*************\n")
+print(linear_regression(vals,notes,0.00000001,1000))
