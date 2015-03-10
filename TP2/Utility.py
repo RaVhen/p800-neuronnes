884b967d1fbf46dce5a10ae787095e3a5d3291ed
Your Name <moi@moi.moi>
2015-03-10 13:28:34 +0100

    csv added

--- TP2/Utility.py
+++ TP2/Utility.py
@@ -5,8 +5,11 @@ Y=
 
 # create a network with two input, two hidden, and one output nodes
 
-n = NNTP.NN(11, 50, 1)
+n = NNTP.NN(11, 50, 1) #couche cachee
 #train it with some patterns
 print "Starting bath training"
 n.train(X,Y,iterations=500,N=0.015,M=0,Lambda=0)  # Train is with Back Propagation Algorithm
-   
+   		
+# TO DO
+# Tracer des courbes en fct du nombre de neuronnes sur la couche cachee
+# 
